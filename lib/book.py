cc30c971a2d5a09658bc08db05b09a0f5cf0691c
Your Name <einsteinkibet0110@gmail.com>
2023-12-02 14:09:52 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,13 @@
 
 class Book:
     pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def is_page_count_integer(self):
+        return isinstance(self.page_count, int)
     
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,14 @@
 
 class Shoe:
     pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = 'New'
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
+
+    def is_size_integer(self):
+        return isinstance(self.size, int)
